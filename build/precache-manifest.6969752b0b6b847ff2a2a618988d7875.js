dff8f456cebb8eaf9f4b55e69e13ef5f076f2307
Aleksandr <a.tarasov@66bit.ru>
2020-05-05 19:18:32 +0500

    vk mini apps init

---
+++ build/precache-manifest.6969752b0b6b847ff2a2a618988d7875.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce224e4eeea6d63accb39df9db04bc7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dba8ea06167c5e8df8d",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "17aca1f62188a186f3bd",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "7dba8ea06167c5e8df8d",
+    "url": "/static/js/2.b1f343d3.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.b1f343d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17aca1f62188a186f3bd",
+    "url": "/static/js/main.4dc07785.chunk.js"
+  },
+  {
+    "revision": "d7962b5607dfedfef29b",
+    "url": "/static/js/runtime-main.fe0165c8.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
