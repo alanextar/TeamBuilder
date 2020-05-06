7a65c7cfdcd0de1fc4b581c8a83abeeb101c9c89
Nazarov <nazarov-alexey@inbox.ru>
2020-05-06 22:35:19 +0500

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import "core-js/features/map";
-import "core-js/features/set";
-import React from "react";
-import ReactDOM from "react-dom";
-import bridge from "@vkontakte/vk-bridge";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-// Init VK  Mini App
-bridge.send("VKWebAppInit");
-
-ReactDOM.render(<App />, document.getElementById("root"));
-if (process.env.NODE_ENV === "development") {
-  import("./eruda").then(({ default: eruda }) => {}); //runtime download
-}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
