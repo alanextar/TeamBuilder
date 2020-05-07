b6792145a6a027ecfafb8b9abc1b7edf473c22f9
Aleksandr Tarasov <a.tarasov@66bit.ru>
2020-05-07 22:04:26 +0500

    пересоздал по https://reactjs.net/tutorials/aspnetcore.html

--- TeamBuilder/Startup.cs
+++ TeamBuilder/Startup.cs
@@ -1,8 +1,14 @@
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,13 +27,14 @@ namespace TeamBuilder
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllersWithViews();
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			services.AddReact();
 
-			// In production, the React files will be served from this directory
-			services.AddSpaStaticFiles(configuration =>
-			{
-				configuration.RootPath = "ClientApp/build";
-			});
+			// Make sure a JS engine is registered, or you will get an error!
+			services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+			  .AddV8();
+
+			services.AddControllersWithViews();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,34 +46,44 @@ namespace TeamBuilder
 			}
 			else
 			{
-				app.UseExceptionHandler("/Error");
+				app.UseExceptionHandler("/Home/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
-
 			app.UseHttpsRedirection();
+
+			// Initialise ReactJS.NET. Must be before static files.
+			app.UseReact(config =>
+			{
+				// If you want to use server-side rendering of React components,
+				// add all the necessary JavaScript files here. This includes
+				// your components as well as all of their dependencies.
+				// See http://reactjs.net/ for more information. Example:
+				//config
+				//  .AddScript("~/js/First.jsx")
+				//  .AddScript("~/js/Second.jsx");
+
+				// If you use an external build too (for example, Babel, Webpack,
+				// Browserify or Gulp), you can improve performance by disabling
+				// ReactJS.NET's version of Babel and loading the pre-transpiled
+				// scripts. Example:
+				//config
+				//  .SetLoadBabel(false)
+				//  .AddScriptWithoutTransform("~/js/bundle.server.js");
+			});
+
+
 			app.UseStaticFiles();
-			app.UseSpaStaticFiles();
 
 			app.UseRouting();
+
+			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllerRoute(
 					name: "default",
-					pattern: "{controller}/{action=Index}/{id?}");
-			});
-
-			app.UseSpa(spa =>
-			{
-				spa.Options.SourcePath = "ClientApp";
-
-				//if (env.IsDevelopment())
-				//{
-				//	spa.UseReactDevelopmentServer(npmScript: "start");
-				//}
-
-				spa.UseReactDevelopmentServer(npmScript: "start");
+					pattern: "{controller=Home}/{action=Index}/{id?}");
 			});
 		}
 	}
