d1152169018c32dd96d984b792f67c3aa4973e2c
milkov <milkov.de@skbkontur.ru>
2020-05-08 22:56:11 +0500

    Рефакторинг publish.bat

--- TeamBuilder/publish.bat
+++ TeamBuilder/publish.bat
@@ -1,16 +1,30 @@
 REM - This file assumes that you have access to the application and that you have docker installed
 REM : Setup your applications name below
 SET APP_NAME="TeamBuilder"
+SET ReleasePath=.\bin\Release\netcoreapp3.1\publish\
 
-REM - Delete all files and folders in publish
-del /q ".\bin\Release\netcoreapp3.1\publish\*"
-FOR /D %%p IN (".\bin\Release\netcoreapp3.1\publish\*.*") DO rmdir "%%p" /s /q
+CALL :CleanPublish %ReleasePath%
+CALL :Rebuild
+CALL :PublishContainer %ReleasePath%
+EXIT /B %ERRORLEVEL%
 
-dotnet clean --configuration Release
+:CleanPublish
+del /q %~1
+FOR /D %%p IN (%~1"*.*") DO rmdir "%%p" /s /q
+EXIT /B 0
+
+:Rebuild
+dotnet clean -c Release
 dotnet publish -c Release
-copy Dockerfile .\bin\Release\netcoreapp3.1\publish\
-cd .\bin\Release\netcoreapp3.1\publish\
+EXIT /B 0
+
+:PublishContainer
+copy Dockerfile %~1
+cd %~1
+
 call heroku container:login
 call heroku container:push web -a vk-team-builder
 call heroku container:release web -a vk-team-builder
-pause
+
+cd ..\..\..\..
+EXIT /B 0
