e4102314414739eb5aecd4d67f9b1fa0c7e27eeb
Nazarov <nazarov-alexey@inbox.ru>
2020-05-14 02:12:47 +0500

    add footer

---
+++ TeamBuilder/ClientApp/src/panels/teams.jsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import { Panel, PanelHeader, Group, FixedLayout, Search } from '@vkontakte/vkui';
+
+const Teams = ({ id, go }) => (
+    <Panel id={id}>
+        <PanelHeader>Команды</PanelHeader>
+        <FixedLayout vertical="top">
+            <Search />
+        </FixedLayout>
+
+    </Panel>
+);
+
+export default Teams;
