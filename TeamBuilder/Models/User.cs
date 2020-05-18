a97e548d1400f9e309e8fa9db0bd0d49f2c3494f
Aleksandr Tarasov <a.tarasov@66bit.ru>
2020-05-18 12:47:10 +0500

    UPDATE-DATABASE!

--- TeamBuilder/Models/User.cs
+++ TeamBuilder/Models/User.cs
@@ -23,8 +23,11 @@ namespace TeamBuilder.Models
 		public string FirstName { get; set; }
 		public string SecondName { get; set; }
 		public string LastName { get; set; }
+		public string City { get; set; }
+		public string About { get; set; }
 		public string FullName => FirstName + " " + SecondName + " " + LastName;
 		public bool IsAvailableForSearch { get; set; }
+		//public bool IsConfirmed { get; set; }
 		public List<UserTeam> UserTeams { get; set; }
 		public List<UserSkill> UserSkills { get; set; }
 	}
