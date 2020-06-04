e4a93bc54b06e4ddaeaa5cae491aef57283af172
Aleksandr Tarasov <alanextar@gmail.com>
2020-06-04 11:45:00 +0500

    селект для отображения

---
+++ TeamBuilder/ClientApp/src/customize.js
@@ -0,0 +1,65 @@
+export const showTokensOnly = {
+    control: (base, state) => ({
+        ...base,
+        backgroundColor: 'transparent',
+        lineHeight: '20px',
+        border: 'none',
+        '&:hover': {
+            border: 'none'
+        },
+        borderRadius: '16px',
+        width: '100%',
+        height: '100%',
+        zIndex: 3,
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        fontSize: '16px',
+        color: 'black',
+    }),
+    container: (provided, state) => ({
+        ...provided,
+        fontSize: '19px',
+    }),
+    dropdownIndicator: (provided, state) => ({
+        ...provided,
+        display: 'none',
+    }),
+    indicatorSeparator: (provided, state) => ({
+        ...provided,
+        display: 'none',
+    }),
+    multiValue: (provided, state) => ({
+        ...provided,
+        borderRadius: '10px',
+    }),
+    multiValueRemove: (provided, state) => ({
+        ...provided,
+        display: 'none',
+    })
+}
+
+export const customCreatableSelect = {
+    control: (base, state) => ({
+        ...base,
+        backgroundColor: 'var(--field_background)',
+        lineHeight: '20px',
+        border: '1px solid var(--field_border)',
+        //'&:hover': {
+        //    border: '1px solid var(--field_border)',
+        //},
+        borderRadius: '16px',
+        width: '100%',
+        height: '100%',
+        zIndex: 3,
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        fontSize: '16px',
+        color: 'black'
+    }),
+    container: (provided, state) => ({
+        ...provided,
+        fontSize: '19px',
+    })
+}
