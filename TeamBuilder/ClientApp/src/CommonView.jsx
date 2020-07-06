e127ebb54fbc2c7e45eff8c863be745825bbb60f
milkov <milkov.de@skbkontur.ru>
2020-07-06 22:10:42 +0500

    refactoring + fix Console errors

--- TeamBuilder/ClientApp/src/CommonView.jsx
+++ TeamBuilder/ClientApp/src/CommonView.jsx
@@ -1,11 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from "redux";
 import { goBack } from "./store/router/actions";
 
-import {
-	View
-} from '@vkontakte/vkui';
+import { View } from '@vkontakte/vkui';
 
 import EventCreate from './panels/events/eventCreate'
 import EventInfo from './panels/events/eventInfo'
@@ -23,15 +20,15 @@ const CommonView = props => {
 	const h = React.createElement;
 
 	const panels = [
-		h(TeamInfo, { id: 'teamInfo' }),
-		h(TeamCreate, { id: 'teamCreate' }),
-		h(TeamEdit, { id: 'teamEdit' }),
-		h(User, { id: 'user' }),
-		h(UserEdit, { id: 'userEdit' }),
-		h(SetUserTeam, { id: 'setUserTeam' }),
-		h(EventInfo, { id: 'eventInfo' }),
-		h(EventCreate, { id: 'eventCreate' }),
-		h(EventEdit, { id: 'eventEdit' })
+		h(TeamInfo, { id: 'teamInfo', key: 'teamInfo' }),
+		h(TeamCreate, { id: 'teamCreate', key: 'teamCreate' }),
+		h(TeamEdit, { id: 'teamEdit', key: 'teamEdit' }),
+		h(User, { id: 'user', key: 'user' }),
+		h(UserEdit, { id: 'userEdit', key: 'userEdit' }),
+		h(SetUserTeam, { id: 'setUserTeam', key: 'setUserTeam' }),
+		h(EventInfo, { id: 'eventInfo', key: 'eventInfo' }),
+		h(EventCreate, { id: 'eventCreate', key: 'eventCreate' }),
+		h(EventEdit, { id: 'eventEdit', key: 'eventEdit' })
 	]
 
 	return (
@@ -49,18 +46,8 @@ const CommonView = props => {
 	);
 };
 
-const mapStateToProps = (state) => {
-	return {
-		profileUser: state.user.profileUser,
-		profile: state.user.profile
-	}
+const mapDispatchToProps = {
+	goBack
 };
 
-function mapDispatchToProps(dispatch) {
-	return {
-		dispatch,
-		...bindActionCreators({ goBack }, dispatch)
-	}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CommonView);
+export default connect(null, mapDispatchToProps)(CommonView);
