c569dea775d28ea9a9f8dfda323103f8ee97acf3
milkov <milkov.de@skbkontur.ru>
2020-07-27 17:26:06 +0500

    Модели под уведомления и сетевой статус пользователя

---
+++ TeamBuilder/Models/Connection.cs
@@ -0,0 +1,18 @@
+﻿namespace TeamBuilder.Models
+{
+	public class Connection
+	{
+		public long Id { get; set; }
+
+		public long UserId { get; set; }
+
+		public ConnectStatus ConnectStatus { get; set; }
+	}
+
+	public enum ConnectStatus
+	{
+		Unknown,
+		Online,
+		Offline
+	}
+}
---
+++ TeamBuilder/Models/Notification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeamBuilder.Models
+{
+	public class Notification
+	{
+		public long Id { get; set; }
+
+		public long UserId { get; set; }
+
+		public DateTime DateTimeNotify { get; set; }
+		public string Message { get; set; }
+		public NotifyType NotifyType { get; set; }
+
+		public Dictionary<string, string> Items { get; set; }
+	}
+
+	public enum NotifyType
+	{
+		None,
+		Regular,
+		Destructive,
+		Important,
+		Service
+	}
+}
